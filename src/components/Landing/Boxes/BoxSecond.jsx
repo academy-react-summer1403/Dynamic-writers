a7491b08c4664c59aedf928a8bd56c7c157c924a
KianJanloo <ddrdslayer@gmail.com>
2024-10-06 22:21:18 +0330

    doing

--- src/components/Landing/Boxes/BoxSecond.jsx
+++ src/components/Landing/Boxes/BoxSecond.jsx
@@ -1,43 +1,16 @@
 import { Button } from '@nextui-org/react'
 import { Note01Icon, StudentIcon } from 'hugeicons-react'
 import React, { useEffect, useState } from 'react'
-import { GetTeachersList } from '../../../core/services/api/teachersTop'
-import Teachers from './Teachers'
 
 const BoxSecond = () => {
-
-  const [teachers, setTeachers] = useState([])
-
-  const getTeachers = async () => {
-
-    const response = await GetTeachersList()
-
-    setTeachers(response.slice(0,4)) 
-
-    console.log(teachers)
-
-  } 
-
-  useEffect(() => {
-    getTeachers()
-  }, [])
 
   return (
     <div className='my-20 w-5/6 mx-auto flex flex-col gap-10 justify-between md:flex-row'>
   
-      {teachers.map((item, index) => {
-
-        return <Teachers
-          key={index}
-          id={item.teacherId}
-          fullName={item.fullName}
-          linkdinProfileLink={item.linkdinProfileLink}
-          pictureAddress={item.pictureAddress}
-          courseCounts={item.courseCounts}
-          newsCount={item.newsCount}
-        />
-
-      })}
+    <div className='border-3 w-80 h-72 rounded-2xl'></div>
+    <div className='border-3 w-80 h-72 rounded-2xl'></div>
+    <div className='border-3 w-80 h-72 rounded-2xl'></div>
+    <div className='border-3 w-80 h-72 rounded-2xl'></div>
 
     </div>
   )
