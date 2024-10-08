708f92976ccc060a671a0f678800fb8e6d85c99a
KianJanloo <ddrdslayer@gmail.com>
2024-10-08 15:37:23 +0330

    ...

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,15 +7,16 @@ import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div className='iranSans font-semibold flex justify-start flex-col gap-32 w-80 my-10 p-5 bg-gray-200 rounded-xl h-fit md:flex-row-reverse' style={{width: '1320px'}}>
-      <div className='flex flex-row-reverse justify-center items-center gap-10 h-full px-2 md:flex-col'>
+    <div className='w-dvw'>
+    <div className='iranSans font-semibold flex flex-col justify-between p-10 w-11/12 my-10 md:w-11/12 mx-auto pb-24 bg-gray-100 rounded-xl h-fit md:flex-row-reverse'>
+      <div className='flex flex-row-reverse justify-center items-center gap-4 h-full px-2 md:flex-col'>
         <div className='flex flex-col-reverse'>
             <img src={Bahr2} className='w-20 h-4 inline' />
             <img src={Bahr} className='size-20 inline' />
         </div>
         <div className='hidden flex-col gap-2 items-end md:flex'>
-            <a href='#' className='cursor-pointer flex border border-gray-200 bg-white text-red-500 px-2 py-1 rounded-full text-base font-semibold gap-2'> اینستاگرام <InstagramIcon className='text-red-500' /> </a>
-            <a href='#' className='cursor-pointer w-fit flex border border-gray-200 bg-white text-blue-500 px-2 py-1 rounded-full text-base font-semibold gap-2'> تلگرام <TelegramIcon className='text-blue-500' /> </a>
+            <a href='#' className='cursor-pointer flex border border-gray-200 bg-white text-red-500 px-2 py-1 rounded-full text-sm font-semibold gap-2'> اینستاگرام <InstagramIcon className='text-red-500' /> </a>
+            <a href='#' className='cursor-pointer w-fit flex border border-gray-200 bg-white text-blue-500 px-2 py-1 rounded-full text-sm font-semibold gap-2'> تلگرام <TelegramIcon className='text-blue-500' /> </a>
         </div>
 
         <div className='flex flex-col justify-center border h-full md:hidden'>
@@ -27,11 +28,11 @@ const Footer = () => {
 
       <div className='flex flex-col justify-center items-end gap-10 h-full px-2'>
         <h2 className='text-xl iranSansBold'> آکادمی کدنویسی بحر </h2>
-        <span style={{direction: 'rtl'}} className='w-72 text-right text-gray-500'> +13 سال سابقه فعالیت در زمینه آموزش کدنویسی از سنین کودکی تا بزرگسال. هدف ما همیشه این بوده که دانشجویان را با مهارت های لازم برای موفقیت در دنیای فناوری و برنامه نویسی مجهز کنیم. </span>
+        <span style={{direction: 'rtl'}} className='w-72 text-right text-sm text-gray-500'> +13 سال سابقه فعالیت در زمینه آموزش کدنویسی از سنین کودکی تا بزرگسال. هدف ما همیشه این بوده که دانشجویان را با مهارت های لازم برای موفقیت در دنیای فناوری و برنامه نویسی مجهز کنیم. </span>
       </div>
 
-      <div className='flex gap-14'>
-        <div className='flex flex-col justify-center items-end gap-3 h-full px-2 border'>
+      <div className='flex gap-20'>
+        <div className='flex flex-col justify-center items-end gap-3 h-full px-2'>
           <h2 className='text-gray-600 text-base mb-10'> صفحات </h2>
           <Link to='' className='whitespace-nowrap'> خانه </Link> 
           <Link to='' className='whitespace-nowrap'> دوره ها </Link> 
@@ -50,8 +51,8 @@ const Footer = () => {
       </div>
 
       <div className='flex gap-10'>
-        <div className='flex flex-col justify-center border h-full'>
-          <div className='bg-gray-300 size-28 justify-center'> نماد اعتماد الکترونیک  </div>
+        <div className='flex flex-col justify-start items-start h-full'>
+          <div className='bg-gray-200 size-28 justify-center'> نماد اعتماد الکترونیک  </div>
         </div>
 
         <div className='flex flex-col gap-2 items-end md:hidden'>
@@ -60,6 +61,7 @@ const Footer = () => {
         </div>
       </div>
 
+    </div>
     </div>
   )
 }
