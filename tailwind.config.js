7f4d578ee86dae151436d8e267f6f28fae2436d4
KianJanloo <ddrdslayer@gmail.com>
2024-10-08 15:27:20 +0330

    doing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
-
+// tailwind.config.js
 const {nextui} = require("@nextui-org/react");
 
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  content: [
+    // ...
+    // make sure it's pointing to the ROOT node_module
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}", "./src/**/*.{jsx,js,tsx,ts}"
+  ],
   theme: {
     extend: {},
   },
