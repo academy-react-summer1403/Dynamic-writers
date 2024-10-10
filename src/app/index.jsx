cdb5b560b9f1c1c05661d649f03903866900b130
KianJanloo <ddrdslayer@gmail.com>
2024-10-10 13:52:10 +0330

    NotFound

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,22 +1,10 @@
-import { useEffect, useState } from 'react'
 import './App.css'
-import axios from 'axios';
-import {getCourserList} from '../core/services/api/cours';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { NextUIProvider } from '@nextui-org/react';
 import NotFoundPage from '../screens/NotFoundPage';
 import Root from '../screens/Root';
 
 function App() {
-
-  const getList = async() => {
-    const result = await getCourserList(5);
-    console.log(result);
-  }
-
-  useEffect(()=>{
-    getList();
-  },[])
 
   const router = createBrowserRouter([
     {
