193aa23dab568aaa88756fb3f8935703aac54853
Roshana <132509624+roshana-401@users.noreply.github.com>
2024-10-11 20:38:16 +0330

    Image and Profile added

---
+++ src/components/NewsDetail/Profile.jsx
@@ -0,0 +1,11 @@
+import Image from './Image'
+const Profile = ({img,name}) => {
+  return (
+    <div className='flex flex-row-reverse gap-4'>
+        <Image image={img} />
+        <span className='font-[700] text-[20px] text-[#272727] leading-[40px]'>{name}</span>
+    </div>
+  )
+}
+
+export default Profile
