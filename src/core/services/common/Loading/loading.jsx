b3e36050874faedb3ff81278b1643a443ef9b0e4
Roshana <132509624+roshana-401@users.noreply.github.com>
2024-10-13 01:12:06 +0330

    loading component added

---
+++ src/core/services/common/Loading/loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CircularProgress from '@mui/material/CircularProgress';
+
+const loading = () => {
+  return (
+    <div className='w-dvw h-[500px] flex justify-center items-center gap-4'>
+    <span>...درحال بارگزاری</span>
+    <CircularProgress />
+  </div>
+  )
+}
+
+export default loading
