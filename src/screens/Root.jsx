bf4ee77d4306d4b1364599f92a9629b56a565fbf
KianJanloo <ddrdslayer@gmail.com>
2024-10-12 20:10:38 +0330

    Root

--- src/screens/Root.jsx
+++ src/screens/Root.jsx
@@ -4,15 +4,9 @@ import RootCom from '../components/Root/RootCom'
 const Root = () => {
   return (
     <div className='iranSans'>
-<<<<<<< HEAD
-     <Header />
 
-     <Outlet />
+      <RootCom /> 
 
-     <Footer />
-=======
-     <RootCom /> 
->>>>>>> e17df0f2989c6b65d650f413132f10dee7e9a086
     </div>
   )
 }
