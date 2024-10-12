5323f062f4c1a7c96e2082fc44e8402ea3d9994f
Roshana <132509624+roshana-401@users.noreply.github.com>
2024-10-13 01:10:08 +0330

    Rate api completed

--- src/common/Rate/Rate.jsx
+++ src/common/Rate/Rate.jsx
@@ -1,18 +1,36 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import { StarCircleIcon} from 'hugeicons-react'
 import Rating from '@mui/material/Rating';
-const Rate = () => {
-  const [Rate, setRate] = useState(-1)
+import NewsRate from '../../core/services/api/News/NewsRate';
+
+import { ToastContainer, toast } from 'react-toastify';
+
+const Rate = ({RateNews,Flag,id}) => {
+
+  const [Rate, setRate] = useState(RateNews)
+  const [flag,setFlag]=useState(Flag)
+
+  const notifySuccess = (massage) => toast.success(massage,{position:"top-center",theme:"dark"});
+
+  
   return (
     <div className='text-[#272727] flex flex-row-reverse gap-5 text-[20px] items-center font-[700]'>
         <StarCircleIcon color='#3772FF'/>
          امتیاز دهید 
          <div className='flex flex-row items-center' >
          <Rating 
-            name="simple-controlled"
+            name="half-rating"
+            defaultValue={0} 
+            precision={0.5}
             value={Rate}
-            onChange={(event, newValue) => {
-              setRate(newValue);
+            readOnly={flag}
+            onChange={async(event, newValue) => {
+              if(flag==false){
+                setRate(newValue);
+                let message = await NewsRate(id,newValue)
+                notifySuccess(message.message)
+                setFlag(true)
+              }
             }}
           />
          </div>
