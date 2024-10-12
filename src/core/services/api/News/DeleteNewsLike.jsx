43efef99571c0f24239375da9dc80f6747fa429f
Roshana <132509624+roshana-401@users.noreply.github.com>
2024-10-12 12:34:22 +0330

    DeleteNewsLike api completed

--- src/core/services/api/News/DeleteNewsLike.jsx
+++ src/core/services/api/News/DeleteNewsLike.jsx
@@ -1,8 +1,14 @@
-import http from '../interceptor'
-
+import http from '../../interceptor'
+import GetNewsById from './GetNewsById';
 const DeleteNewsLike = async(id) => {
     try{
-        await http.delete('/News/DeleteLikeNews');
+        let News = await GetNewsById(id)
+        console.log(News.detailsNewsDto.likeId)
+        let response=await http.delete('/News/DeleteLikeNews',{
+            data: {
+                deleteEntityId: News.detailsNewsDto.likeId
+            }})
+        return response
 
     }catch(er){
         console.log(er)
