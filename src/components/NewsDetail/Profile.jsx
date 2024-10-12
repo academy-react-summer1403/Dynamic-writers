e05965ac451936551ecb11bc511d31642246514a
Roshana <132509624+roshana-401@users.noreply.github.com>
2024-10-13 01:13:01 +0330

    responsive compelete

--- src/components/NewsDetail/Profile.jsx
+++ src/components/NewsDetail/Profile.jsx
@@ -3,7 +3,7 @@ const Profile = ({img,name}) => {
   return (
     <div className='flex flex-row-reverse gap-4'>
         <Image image={img} />
-        <span className='font-[700] text-[20px] text-[#272727] leading-[40px]'>{name}</span>
+        <span className='font-[700] text-[20px] text-[#272727] leading-[40px] text-right max-lg:text-[17px] max-sm:text-[20px]'>{name}</span>
     </div>
   )
 }
