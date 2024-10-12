afe62b67c21efa6ce4979b834943c8127283e72a
Roshana <132509624+roshana-401@users.noreply.github.com>
2024-10-13 01:10:37 +0330

    Option conect to api

--- src/components/NewsDetail/Options.jsx
+++ src/components/NewsDetail/Options.jsx
@@ -1,14 +1,15 @@
-import React, {useState } from 'react'
+import React, {useState,useEffect } from 'react'
 import { ThumbsUpIcon,ThumbsDownIcon,BookmarkAdd02Icon } from 'hugeicons-react'
 import NewsLike from '../../core/services/api/News/NewsLike'
 import DeleteNewsLike from '../../core/services/api/News/DeleteNewsLike'
 import NewsDisLike from '../../core/services/api/News/NewsDisLike'
 import AddFavoriteNews from '../../core/services/api/News/AddFavoriteNews'
+import DeleteFavoriteNews from '../../core/services/api/News/DeleteFavoriteNews'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Options = ({News}) => {
-    const [checkClick, setcheckClick] = useState({like:false,dislike:false,save:false})
+    const [checkClick, setcheckClick] = useState({like:News.detailsNewsDto.currentUserIsLike,dislike:News.detailsNewsDto.currentUserIsDissLike,save:News.detailsNewsDto.isCurrentUserFavorite})
     const notifySuccess = (massage) => toast.success(massage,{position:"top-center",theme:"dark"});
     const notifyError = () => toast.warn("شما نمیتوانید رای خود را برگردانید",{position:"top-center",theme:"dark"});
 
@@ -25,7 +26,7 @@ const Options = ({News}) => {
       else if(Name=='like'){
           if(checkClick.like==true){
             setcheckClick({...checkClick,like:!checkClick.like,dislike:false})
-            let message=await DeleteNewsLike(News.detailsNewsDto.likeId)
+            let message=await DeleteNewsLike(News.detailsNewsDto.id)
             notifySuccess(message.message)
           }else{
             setcheckClick({...checkClick,like:!checkClick.like,dislike:false})
@@ -46,15 +47,16 @@ const Options = ({News}) => {
         }
       }
     }
+    
   return (
     <div className='flex flex-row gap-2'>
-       <div className={`w-[56px] h-[56px] rounded-full border-[#E4E4E4] border-2 ${checkClick.dislike ? "bg-black":""} flex items-center justify-center cursor-pointer hover:scale-110 transition-all`}  onClick={()=>CheckClick('dislike')}>
+       <div className={`w-[56px] h-[56px] rounded-[100%] border-[#E4E4E4] border-2 ${checkClick.dislike ? "bg-black":""} flex items-center justify-center cursor-pointer hover:scale-110 transition-all max-lg:w-[46px] h-[46px] max-sm:w-[56px] h-[56px]`}  onClick={()=>CheckClick('dislike')}>
             <ThumbsDownIcon color={`${checkClick.dislike ? "white":"black"}`} />
         </div>
-        <div className={`w-[56px] h-[56px] rounded-full border-[#E4E4E4] ${checkClick.like ? "bg-black":""} border-2 flex items-center justify-center cursor-pointer hover:scale-110 transition-all`}  onClick={()=>CheckClick('like')}>
+        <div className={`w-[56px] h-[56px] rounded-[100%] border-[#E4E4E4] ${checkClick.like ? "bg-black":""} border-2 flex items-center justify-center cursor-pointer hover:scale-110 transition-all max-lg:w-[46px] h-[46px] max-sm:w-[56px] h-[56px]`}  onClick={()=>CheckClick('like')}>
             <ThumbsUpIcon color={`${checkClick.like ? "white":"black"}`}/>
         </div>
-        <div className={`w-[56px] h-[56px] rounded-full border-[#E4E4E4] border-2 ${checkClick.save ? "bg-black":""} flex items-center justify-center cursor-pointer hover:scale-110 transition-all`}  onClick={()=>CheckClick('save')}>
+        <div className={`w-[56px] h-[56px] rounded-[100%] border-[#E4E4E4] border-2 ${checkClick.save ? "bg-black":""} flex items-center justify-center cursor-pointer hover:scale-110 transition-all max-lg:w-[46px] h-[46px] max-sm:w-[56px] h-[56px]`}  onClick={()=>CheckClick('save')}>
             <BookmarkAdd02Icon color={`${checkClick.save ? "white":"black"}`}/>
         </div>
         <ToastContainer/>
