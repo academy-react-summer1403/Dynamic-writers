97c74147d6aa92035e0ec65867762be24e4cca80
KianJanloo <ddrdslayer@gmail.com>
2024-10-12 18:51:32 +0330

    Develop

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,16 +1,68 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import axios from 'axios';
-import {getCourserList} from '../core/services/api/cours';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { NextUIProvider } from '@nextui-org/react';
+import Login from '../screens/Login/Login';
+import Verify from '../screens/Login/verify';
+import Register from '../screens/Register/Register';
+import VerifyRegister from '../screens/Register/VerifyRegister';
+import RegisterStep3 from '../screens/Register/RegisterStep3';
+import ForgetPassword from '../screens/ResetPassword/ForgetPassword';
+import ResetPassword from '../screens/ResetPassword/ResetPassword';
+import NotFoundPage from '../screens/NotFoundPage';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from '../screens/Root';
+import CourseListRout from '../screens/CourseListRout';
+import News from '../screens/News';
 
 function App() {
 
   const router = createBrowserRouter([
     {
-      path: '/'
+      path: '/login',
+      element: <Login />
+    },
+    {
+      path: '/verify',
+      element: <Verify />
+    },
+    {
+      path: '/register',
+      element: <Register />
+    },
+    {
+      path: '/verifyRegister',
+      element: <VerifyRegister />
+    },
+    {
+      path: '/registerAccount',
+      element: <RegisterStep3 />
+    },
+    {
+      path: '/forgetPassword',
+      element: <ForgetPassword />
+    },
+    {
+      path: '/resetPassword/:configValue',
+      element: <ResetPassword />
+    },
+    {
+      path: '*',
+      element: <NotFoundPage />
+    },
+    {
+      path: '/',
+      element: <Root />,
+      children: [
+
+        {
+          path: '/courseList',
+          element: <CourseListRout />
+        },
+        {
+          path: '/newsList',
+          element: <News />
+        }
+      ]
     }
+
   ])
 
   return <NextUIProvider>
