5f17841951e65831a9352bf0fbf1b00d6493612b
KianJanloo <ddrdslayer@gmail.com>
2024-10-22 20:22:06 +0330

    Panel

--- src/core/services/interceptor/index.js
+++ src/core/services/interceptor/index.js
@@ -12,29 +12,28 @@ const onSuccess = (response) => {
 }
 
 const onError = (err) => {
-   if(err.response) {
-      const status = err.response.status
+   // if(err.response) {
+   //    const status = err.response.status
 
-      if(status === 401) {
-         removeItem('token')
-         window.location.pathname = '/Error401'
-      }
+   //    if(status === 401) {
+   //       removeItem('token')
+   //       window.location.pathname = '/Error401'
+   //    }
    
-      if(status === 403) {
-         window.location.pathname = '/Error403'
-      }
+   //    if(status === 403) {
+   //       window.location.pathname = '/Error403'
+   //    }
       
-      if(status === 408) {
-         window.location.pathname = '/Error408'
-      }
+   //    if(status === 408) {
+   //       window.location.pathname = '/Error408'
+   //    }
       
-      if(status === 500) {
-         window.location.pathname = '/Error500'
-      }
-   }
-   else{
-      // console.log(err.request)
-   }
+   //    if(status === 500) {
+   //       window.location.pathname = '/Error500'
+   //    }
+   // }
+
+   return 
 }
 
 instance.interceptors.response.use(onSuccess, onError)
