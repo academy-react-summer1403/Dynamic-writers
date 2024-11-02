dc4e4c184ce9ab73edbb901c8ba9b82d68896db4
KianJanloo <ddrdslayer@gmail.com>
2024-11-02 14:49:04 +0330

    Panel

--- src/components/Panel/Panel.jsx
+++ src/components/Panel/Panel.jsx
@@ -36,13 +36,10 @@ const Panel = ({profile,setRerender}) => {
     return (
     <div dir='ltr' className='relative md:w-[100%] lg:w-[100%] dark:bg-slate-950 rounded-[16px] border-4 border-blue-500 dark:border-blue-900 bg-white h-auto mx-auto mt-[30px]'>
         <div className='w-[100%] md:h-[113px] dark:bg-blue-900 h-[100px] bg-[#2A67F9]'></div>
-        <motion.div transition={{duration: 3}} animate={{
-            y: [-100, 0],
-            rotate: [360, 0, 0, 360, 0],
-        }} className='rounded-[100%] w-[128px] h-[128px] border-[7px] dark:bg-blue-900 bg-[#2A67F9] absolute md:top-[-45px] top-[-45px] right-8 overflow-hidden'>
+        <div className='rounded-[100%] w-[128px] h-[128px] border-[7px] dark:bg-blue-900 bg-[#2A67F9] absolute md:top-[-45px] top-[-45px] right-8 overflow-hidden'>
             <img className='w-[100%] h-[100%] bg-purple-400 dark:bg-blue-800' src={profile.currentPictureAddress}/>
             
-        </motion.div>
+        </div>
         <div className='border-[3px] border-white bg-[#2A67F9] w-[24px] h-[24px] absolute md:top-[50px] top-[50px] right-12 flex justify-center items-center cursor-pointer rounded-[100%]' >
                 <ImageAdd02Icon color='white' size={15} onClick={()=>{navigate("AddProfileImage");scrollToSection();}}/>
          </div>
